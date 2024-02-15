1fc46e8c5380a1c001a80c5fe6b9c4dade37894d
juan.tobon <juan.tobon@syscafe.com>
2024-02-15 08:31:11 -0500

    Flutter bloc

---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => {}, 
+            icon: const Icon(Icons.refresh_outlined)
+          )
+        ],
+        title: const Text('Bloc counter'),
+      ),
+      body: const Center(
+        child: Text('Counter value: xxxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            child: const Text('+3'),
+            onPressed: () => {}
+          ),
+          const SizedBox(height: 15,),
+          FloatingActionButton(
+            heroTag: '2',
+            child: const Text('+2'),
+            onPressed: () => {}
+          ),
+          const SizedBox(height: 15,),
+          FloatingActionButton(
+            heroTag: '3',
+            child: const Text('+1'),
+            onPressed: () => {}
+          ),
+        ],
+      ),
+    );
+  }
+}
